620e112aaecdc5277e8980bc4f05bd54b387097e
jagadeesh177 <easalajagadeesh@gmail.com>
2025-08-28 10:17:17 +0530

    added

---
+++ db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/admin';
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+    });
